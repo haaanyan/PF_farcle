741f40359cb3be9ed78cbb7d349e837542d83a85
haaanyan <kiha.0327@icloud.com>
2021-06-07 11:35:34 +0000

    [make]post_images

---
+++ db/migrate/20210607075005_add_caption_to_post_images.rb
@@ -0,0 +1,7 @@
+class AddCaptionToPostImages < ActiveRecord::Migration[5.2]
+  def change
+    add_column :post_images, :caption, :text
+    add_column :post_images, :image_id, :string
+    add_column :post_images, :user_id, :integer
+  end
+end
