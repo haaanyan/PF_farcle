3afec7032ccdb14c1b9eb9c9e8b405b15748ac4a
haaanyan <kiha.0327@icloud.com>
2021-06-07 01:47:08 +0000

    [make]show_edit_page

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '8a3be3d0642b6414bb1cb9503886e66520b88c61e863bcf867cbd17781584427ff7c134df9007fa7639b38a3699a7c5210de91d71a25d5ad4d0cac9df09bd178'
