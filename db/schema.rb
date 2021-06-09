79aaa52ae96527d8a04fe615863dfb89c0abd230
haaanyan <kiha.0327@icloud.com>
2021-06-09 05:47:50 +0000

    [Add]relationships

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_07_075005) do
+ActiveRecord::Schema.define(version: 2021_06_08_065555) do
 
   create_table "board_comments", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 2021_06_07_075005) do
   create_table "relationships", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "following_id"
+    t.integer "follower_id"
   end
 
   create_table "users", force: :cascade do |t|
