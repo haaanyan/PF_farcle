0b261ae18977ed9067002d3a39359d15202551fa
haaanyan <kiha.0327@icloud.com>
2021-06-10 06:46:23 +0000

    [make]feature-favorite

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,2 +1,8 @@
 class Favorite < ApplicationRecord
+  validates :user_id, presence: true, uniqueness: {scope: :post_image_id}
+  validates :post_image_id, presence: true
+
+  belongs_to :user
+  belongs_to :post_image
+
 end
---
+++ db/migrate/20210609081853_add_user_id_to_favorites.rb
@@ -0,0 +1,6 @@
+class AddUserIdToFavorites < ActiveRecord::Migration[5.2]
+  def change
+    add_column :favorites, :user_id, :integer
+    add_column :favorites, :post_image_id, :integer
+  end
+end
