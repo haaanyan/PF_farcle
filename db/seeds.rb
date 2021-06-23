5f0dc6a7961a34213e3850adedda8ae5d4907941
haaanyan <kiha.0327@icloud.com>
2021-06-23 04:44:41 +0000

    [Add]フォロー用seedデータ

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,51 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+Faker::Config.locale = :ja
+
+User.create!(
+  name: "テストアカウント",
+  introduction: "宜しくお願いします",
+  email: "test_user1@test.com",
+  password: "password",
+  profile_image: File.open("#{Rails.root}/user_image/profile_image.jpeg"),
+  prefecture: 2,
+  product: "トマト",
+  history: "5"
+)
+
+10.times do |n|
+  User.create!(
+    name: Faker::Name.name ,
+    introduction: "よろしくお願いします!",
+    email: Faker::Internet.email,
+    password: "testtest",
+    profile_image: File.open("#{Rails.root}/user_image/user_image#{n}.jpeg"),
+    prefecture: rand(1..47),
+    product: "米",
+    history:Faker::Number.number(digits: 1)
+  )
+end
+
+10.times do |n|
+  PostImage.create!(
+    caption: Faker::Lorem.unique.paragraph,
+    user_id: User.find(n+1).id,
+    image: File.open("#{Rails.root}/seed_image/seed_image(#{n}).jpeg")
+    )
+end
+
+if Rails.env.production?
+  RAND_FUNC = 'RAND()'
+else
+  RAND_FUNC = 'RANDOM()'
+end
+
+User.all.each do |user|
+  follow_users = User.order(RAND_FUNC).limit(rand(1..10))
+  follow_users.each do |follow_user|
+    user.follow(follow_user)
+  end
+end
