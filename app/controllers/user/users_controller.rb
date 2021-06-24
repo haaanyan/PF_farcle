07e046461ee06326fec44d43bd207d7091c59fe6
haaanyan <kiha.0327@icloud.com>
2021-06-24 06:19:54 +0000

    [Fix]バリデーション

--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -1,5 +1,6 @@
 class User::UsersController < ApplicationController
   before_action :authenticate_user!
+
   def show
     @user = User.find(params[:id])
     @post_images = PostImage.where(user_id: @user.id)
@@ -7,10 +8,14 @@ class User::UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    if @user == current_user
+      render :edit
+    else
+      redirect_to root_path
+    end
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "プロフィール情報を変更しました"
       redirect_to user_path(current_user)
